dee4c35cf1e9cec7344141af9533e90481016c9f
jorgealexis07 <201711104@tese.edu.mx>
2022-04-26 13:08:32 -0500

    DELETE Crea un endpoint para eliminar un explorer

--- app.js
+++ app.js
@@ -40,6 +40,14 @@ app.put('/v1/explorers/:id', (req, res) => {
     res.status(200).json({message: "Updated!"})
 })
 
+// DELETE Crea un endpoint para eliminar un explorer
+app.delete('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers DELETE request ${new Date}`)
+    console.log(`Delete explorer with id ${req.params.id}`)
+    const requestBody = req.body //parametros de un cliente
+    res.status(200).json({message: "Deleted"})
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
      console.log(`Example app listening on port ${port}`)
