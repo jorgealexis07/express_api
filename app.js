089fc5dbac541fe1518e0032f7f57f7a7ee538a5
jorgealexis07 <201711104@tese.edu.mx>
2022-04-26 12:59:03 -0500

    // POST Crea un endpoint que se encargue de crear un explorer

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.json()) //Indicamos que usaremos JSON
 const port = 3000
 
 // HTTP METHODS
+//GET Crea un endpoint que regrese una lista de explorers
 app.get('/v1/explorers', (req,res) => {
     console.log(`Api Explorers GET ALL requests ${new Date()}`)
     const explorer1 = {id: 1, name: "Alexis1"}
@@ -16,12 +17,19 @@ app.get('/v1/explorers', (req,res) => {
     const explorers = [explorer1, explorer2, explorer3, explorer4]
     res.status(200).json(explorers)
 })
-
+//GET Crea un endpoint que te regrese un explorer mediando un ID
 app.get('/v1/explorers/:id', (req, res) => {
     console.log(`Api Explorers GET request ${new Date}`)
     console.log(`Getting explorer with id ${req.params.id}`)
     const explorer = {id: 1, name: "Alexis"}
     res.status(200).json(explorer )
+})
+
+// POST Crea un endpoint que se encargue de crear un explorer
+app.post('/v1/explorers', (req,res) => {
+    console.log(`Api Explorers POST request ${new Date()}`)
+    const requestBody = req.body //parametros de un cliente
+    res.status(201).json({message: "Created"})
 })
 
 // Con esto inicializamos esta app
